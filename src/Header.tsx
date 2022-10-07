ceac8402ff25b80440f018c0dafd819d15f768b4
Udaloiii <udaloiiisohappy@gmail.com>
2022-10-07 20:47:50 +0300

    Create Header Component

---
+++ src/Header.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export function Header() {
+    return (
+        <div className="header">
+            <img
+                src="https://img.freepik.com/free-psd/black-ink-text-embossed_125540-1111.jpg?w=2000&t=st=1663787285~exp=1663787885~hmac=d12245bb2df6e19352beaf1b0d985fd9f77e6a12f6ee5a87f502f6aaf9a2138f"/>
+        </div>
+    )
+}
