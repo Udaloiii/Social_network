5c8499c6ac1875944f681943338e328f5f05f07c
Udaloiii <udaloiiisohappy@gmail.com>
2022-10-07 20:48:06 +0300

    Create Profile Component

---
+++ src/Profile.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+export function Profile() {
+    return (
+        <div className="content">
+            <div>
+                <img
+                    src="https://img.freepik.com/free-vector/digital-marketing-infographic_52683-9003.jpg?w=1480&t=st=1663790524~exp=1663791124~hmac=2cdb763077c25c8d4c6f534d174116fcab0a06ba16d8c26ffad5db22969a12d2"/>
+                Main content
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                My posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    <div>post 1</div>
+                    <div>post 2</div>
+                </div>
+            </div>
+        </div>
+    )
+}
